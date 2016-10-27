e302f4a4f58a52569c9c2fa5c76cbd606c0af995
luoyu1 <luoyu1@ymt360.com>
2016-10-27 18:12:35 +0800

    index

---
+++ generators/app/index.js
@@ -0,0 +1,79 @@
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+    _generateBasicFiles: function(props) { //copy files
+        this.log('-------start to generate files--------')
+
+        this.copy(props.jslib + '_bower.json', 'bower.json');
+
+
+        if (props.grunt) {
+            this.directory(props.jslib + '_app', 'app');
+            this.directory('assets', 'assets');
+
+            this.copy('README.md', 'README.md');
+
+            this.copy('package.json', 'package.json');
+            this.copy('Gruntfile.js', 'Gruntfile.js');
+        } else {
+            this.directory('simple-admin', 'app');
+        }
+
+    },
+    prompting: function() {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the ' + chalk.red('generator-ymt-admin') + ' generator!'
+        ));
+
+        var prompts = [{
+            type: 'list',
+            name: 'jslib',
+            message: 'choose a framework or library you need?(default jquery)',
+            choices: ['jquery', 'angular'],
+            default: 'jquery'
+        }, {
+            type: 'confirm',
+            name: 'grunt',
+            message: 'do you need grunt?(default no)',
+            default: false
+        }, {
+            type: 'list',
+            name: 'theme',
+            message: 'which theme do you like?(default theme blue)',
+            choices: ['blue', 'purple', 'yellow', 'green'],
+            default: 'blue' //
+        }];
+
+        return this.prompt(prompts).then(function(props) {
+            // To access props later use this.props.jslib;
+            this.props = props;
+            this._generateBasicFiles(this.props);
+        }.bind(this));
+    },
+
+    writing: function() {
+        //templates
+        if (this.props.grunt) {
+            this.fs.copyTpl(
+                this.templatePath(this.props.jslib + '_index.html'),
+                this.destinationPath('app/index.html'), { style: this.props.theme }
+            );
+        } else {
+            this.fs.copyTpl(
+                this.templatePath('simple_index.html'),
+                this.destinationPath('app/index.html'), { style: this.props.theme }
+            );
+        }
+
+    },
+
+    install: function() {
+        if (this.props.grunt) {
+            this.installDependencies();
+        }
+        this.log('finished!')
+    }
+});
